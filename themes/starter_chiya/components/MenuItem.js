afa2059b47d708c5460893f7b4ac0d8ec96f3a94
Elton <a245900@hotmail.com>
2025-03-20 18:05:02 +0800

    滑鼠指到菜單會自動開啟子菜單

--- themes/starter_chiya/components/MenuItem.js
+++ themes/starter_chiya/components/MenuItem.js
@@ -40,8 +40,9 @@ export const MenuItem = ({ link }) => {
       {/* 有子菜单的 MenuItem */}
       {hasSubMenu && (
         <li className='submenu-item group relative whitespace-nowrap'>
-          <button
-            onClick={toggleSubMenu}
+          <div
+            onMouseOver={() => setIsSubMenuOpen(true)}
+            onMouseOut={() => setIsSubMenuOpen(false)}
             className={`cursor-pointer relative px-8 flex items-center justify-between py-2 text-base font-medium text-dark group-hover:text-primary dark:text-white lg:ml-8 lg:mr-0 lg:inline-flex lg:py-6 lg:pl-0 lg:pr-4 ${
               router.route === '/'
                 ? 'lg:text-white lg:group-hover:text-white'
@@ -61,14 +62,14 @@ export const MenuItem = ({ link }) => {
               xmlns='http://www.w3.org/2000/svg'>
               <path d='M7.99999 14.9C7.84999 14.9 7.72499 14.85 7.59999 14.75L1.84999 9.10005C1.62499 8.87505 1.62499 8.52505 1.84999 8.30005C2.07499 8.07505 2.42499 8.07505 2.64999 8.30005L7.99999 13.525L13.35 8.25005C13.575 8.02505 13.925 8.02505 14.15 8.25005C14.375 8.47505 14.375 8.82505 14.15 9.05005L8.39999 14.7C8.27499 14.825 8.14999 14.9 7.99999 14.9Z' />
             </svg>
-          </button>
+          </div>
 
           {/* 子菜单 */}
           <div
             className={`submenu dark:border-gray-600 relative left-0 top-full w-[250px] rounded-sm bg-white p-4 transition-all duration-300 dark:bg-dark-2 lg:absolute lg:shadow-lg ${
               isSubMenuOpen
-                ? 'block opacity-100 visible'
-                : 'hidden opacity-0 invisible'
+                ? 'visible opacity-100 top-12'
+                : 'invisible opacity-0 top-10'
             }`}>
             {link.subMenus.map((sLink, index) => (
               <Link
