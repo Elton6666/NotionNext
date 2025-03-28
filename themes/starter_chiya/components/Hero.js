5097509a44f97124e28521351e7636f5ae99e4fa
Elton <a245900@hotmail.com>
2025-03-28 13:01:17 +0800

    背景改黑色

--- themes/starter_chiya/components/Hero.js
+++ themes/starter_chiya/components/Hero.js
@@ -22,7 +22,7 @@ export const Hero = props => {
       {/* <!-- ====== Hero Section Start --> */}
       <div
         id='home'
-        className='relative overflow-hidden bg-primary pt-[120px] md:pt-[130px] lg:pt-[160px]'>
+        className='relative overflow-hidden bg-black pt-[120px] md:pt-[130px] lg:pt-[160px]'>
         {/* 背景圖 */}
         {backgroundEnabled && backgroundImage && (
           <LazyImage 
